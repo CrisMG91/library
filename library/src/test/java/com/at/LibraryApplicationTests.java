302e3344f134b7ed6000d65e59e1b9d41a380796
Cris <Cris@Cris-PC>
2016-08-31 01:21:44 +0200

    Test en comentario

--- library/src/test/java/com/at/LibraryApplicationTests.java
+++ library/src/test/java/com/at/LibraryApplicationTests.java
@@ -8,4 +8,14 @@ public class LibraryApplicationTests {
 	public void contextLoads() {
 	}
 
+	/*@Test 
+	public void createTest() throws InterruptedException{
+		final List<Book> books = new List<>();
+		bookDao.findAll().forEach(b -> books.add(b));
+		final User u = userDao.findOne(l);
+		for(int i=0; i<100000; i++){
+			
+		}
+		
+	}*/
 }
